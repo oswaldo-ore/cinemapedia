58ae31a725451c8a1fd15f404a217a61496bf549
Oswaldo Orellana <oswaldo.orellana.v@gmail.com>
2024-01-03 14:03:56 -0400

    environment

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
-import 'package:cinemapedia/config/router/app_router.dart';
-import 'package:cinemapedia/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:cinemapedia/config/router/app_router.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'package:cinemapedia/config/theme/app_theme.dart';
+
+void main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MainApp());
 }
 
